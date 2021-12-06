ef3aa45b36535298e25a4d66b142c8cb3bdf0cb1
lorenascimentoo <lorenanascimento28@gmail.com>
2021-12-06 18:28:19 -0300

    cópia da atp77

---
+++ atividadep78/atp78/src/app/tp-cliente-lista/tp-cliente-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tp-cliente-lista',
+  templateUrl: './tp-cliente-lista.component.html',
+  styleUrls: ['./tp-cliente-lista.component.css']
+})
+export class TpClienteListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
